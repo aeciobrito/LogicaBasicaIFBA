721219a19db337bb3447f1e9c541c7e234e55352
Aecio Desktop <aeciobrito@gmail.com>
2019-05-16 23:24:30 -0300

    Primeiro commit. Criação dos projetos vazios

---
+++ CollectionsDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
