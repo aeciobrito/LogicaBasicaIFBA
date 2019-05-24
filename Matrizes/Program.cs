66a116957df4436d0d54c05e2d200091c97d2cff
Aécio Desktop <aeciobrito@gmail.com>
2019-05-24 00:20:14 -0300

    Adicionado exemplo para vetores e matrizes

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Matrizes são arranjos bi dimensionais
+ * Arranjo é uma estrutura de dados homogênea, ordenada e alocada em um único bloco
+ * Ele possuí tamanho fixo que pode ser determinado em sua declaração.
+ */
+
 namespace Matrizes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[,] matriz = new int[3, 4]; //Declaração de uma matriz com 3 linhas e 4 colunas
+            //As linhas serão de 0 a 2 e as colunas de 0 a 3
+
+
+            matriz[0, 0] = 20; //Insere o valor 20 na linha 0 e coluna 0 da matriz
+            matriz[2, 3] = 12; //Insere o valor 12 na última linha e última coluna da matriz
+
+            Console.WriteLine(matriz[2, 3] + matriz[0, 0]); //Exibe a soma dos valores inseridos nessas posições
         }
     }
 }
--- Vetores/Program.cs
+++ Vetores/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Vetor é um arranjo unidimensional
+ * Arranjo é uma estrutura de dados homogênea, ordenada e alocada em um único bloco
+ * Ele possuí tamanho fixo que pode ser determinado em sua declaração.
+ */
+
 namespace Vetores
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] nota = new int[4]; //Declaração de um vetor com 4 posições de números inteiros
+
+            //A contagem das posições começa do zero. Por ter 4 posições, será do 0 ao 3
+            nota[0] = int.Parse(Console.ReadLine());
+            nota[1] = int.Parse(Console.ReadLine());
+            nota[2] = int.Parse(Console.ReadLine());
+            nota[3] = int.Parse(Console.ReadLine());
         }
     }
 }
