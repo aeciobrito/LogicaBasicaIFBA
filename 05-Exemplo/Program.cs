2d9054c1afb67d9fe68f64b2dbeb8f6f3840696f
Aécio Desktop <aeciobrito@gmail.com>
2019-05-24 00:26:54 -0300

    Ajustes finais

---
+++ 05-Exemplo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respostaLiga, respostaAnda; //Decaração das variáveis para ler as respostas do usuário
+
+            Console.WriteLine("Seu carro liga?"); //Escreve a pergunta
+            respostaLiga = Console.ReadLine(); //Guarda a resposta na variável           
+
+            if (respostaLiga == "sim") //A primeira condicional verifica a resposta principal
+            {
+                Console.WriteLine("Seu carro anda?"); //Essa pergunta só será escrita se o carro não ligar
+                respostaAnda = Console.ReadLine(); //Guarda a resposta na variável
+
+                if (respostaAnda == "sim") //Análise da segunda resposta
+                {
+                    Console.WriteLine("Não foram identificados problemas"); //Nesse caso as duas respostas foram "sim"
+                }
+                else
+                {
+                    Console.WriteLine("Veículo sem combustível"); //Primeira resposta sim, segunda não. Carro liga e não anda
+                }
+
+            }
+            else //Condição negativa, o carro nem ligou
+            {
+                Console.WriteLine("Problemas na bateria!"); //Escreve resultado
+            }
+        }
+    }
+}
--- CondicionaisBasico/Program.cs
+++ CondicionaisBasico/Program.cs
@@ -4,35 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Condicionais que verificam lógica booleana simples, com verdadeiro e falso 
+ */
+
 namespace CondicionaisBasico
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string respostaLiga, respostaAnda; //Decaração das variáveis para ler as respostas do usuário
-
-            Console.WriteLine("Seu carro liga?"); //Escreve a pergunta
-            respostaLiga = Console.ReadLine(); //Guarda a resposta na variável           
-
-            if (respostaLiga == "sim") //A primeira condicional verifica a resposta principal
+            if(true) //altere esse valor para true ou false
             {
-                Console.WriteLine("Seu carro anda?"); //Essa pergunta só será escrita se o carro não ligar
-                respostaAnda = Console.ReadLine(); //Guarda a resposta na variável
-
-                if (respostaAnda == "sim") //Análise da segunda resposta
-                {
-                    Console.WriteLine("Não foram identificados problemas"); //Nesse caso as duas respostas foram "sim"
-                }
-                else
-                {
-                    Console.WriteLine("Veículo sem combustível"); //Primeira resposta sim, segunda não. Carro liga e não anda
-                }
-
+                Console.WriteLine("Esse valor será exibido se o valor de parâmetro for 'true'");
             }
-            else //Condição negativa, o carro nem ligou
+            else
             {
-                Console.WriteLine("Problemas na bateria!"); //Escreve resultado
+                Console.WriteLine("Esse valor será exibido se o valor de parâmetro for 'false'");
             }
         }
     }
